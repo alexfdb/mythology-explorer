73897efdcaaefe9b6e0d2886c5da7f709ad819b3
Alexander Faustino Diaz Bautista <AlexanderFaustinoDiazBautista@Gmail.com>
2025-04-05 22:37:32 +0100

    Se crea el cambio de idioma #9

--- src/main/java/com/mythologi_explorer/controller/ExplorarController.java
+++ src/main/java/com/mythologi_explorer/controller/ExplorarController.java
@@ -1,9 +1,11 @@
 package com.mythologi_explorer.controller;
 
+import com.mythologi_explorer.controller.idioma.IdiomaController;
 import com.mythologi_explorer.controller.pantalla.PantallaController;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 /**
  * @author alexfdb
@@ -12,16 +14,25 @@ import javafx.scene.control.Button;
 public class ExplorarController extends PantallaController {
 
     @FXML
-    private Button buttonExplorar;
+    private Button buttonIdioma;
     @FXML
     private Button buttonUsuario;
+    @FXML
+    private Text textExplorar;
+    @FXML
+    private Text textSumeria;
+    @FXML
+    private Text textEgipcia;
+
+    private String idiomaActual = "es";
 
     /**
      * Cambia a la pantalla de explorar.
      */
     @FXML
-    public void buttonExplorarClick() {
-        pantallaExplorar(buttonExplorar);
+    public void buttonIdiomaClick() {
+    idiomaActual = idiomaActual.equals("es") ? "en" : "es";
+    cambiarIdioma();
     }
 
     /**
@@ -30,6 +41,14 @@ public class ExplorarController extends PantallaController {
     @FXML
     public void buttonUsuarioClick() {
         pantallaIniciar(buttonUsuario);
+    }
+
+    private void cambiarIdioma() {
+        String path = "src/main/resources/view/idioma/" + idiomaActual + ".properties";
+        IdiomaController.ConfigProperties.setPath(path);
+            textExplorar.setText(IdiomaController.ConfigProperties.getProperties("textExplorar"));
+            textSumeria.setText(IdiomaController.ConfigProperties.getProperties("textSumeria"));
+            textEgipcia.setText(IdiomaController.ConfigProperties.getProperties("textEgipcia"));
     }
 
 }
---
+++ src/main/java/com/mythologi_explorer/controller/idioma/IdiomaController.java
@@ -0,0 +1,41 @@
+package com.mythologi_explorer.controller.idioma;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+/**
+ * @author alexfdb
+ * @version 1.0.0
+ */
+public class IdiomaController {
+
+    private IdiomaController() {
+    }
+
+    public static class ConfigProperties {
+
+        private ConfigProperties() {
+        }
+
+        static String path;
+        private static final Properties PROPERTIES = new Properties();
+
+        public static String getProperties(String key) {
+            return PROPERTIES.getProperty(key);
+        }
+
+        public static void setPath(String rutaPath) {
+            path = rutaPath;
+            try (FileInputStream input = new FileInputStream(path);
+                    InputStreamReader isr = new InputStreamReader(input, StandardCharsets.UTF_8);) {
+                PROPERTIES.load(isr);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
